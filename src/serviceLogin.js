16ce817f04f8d9cc79caf5c79f52bc7ce7d1ae12
kasvrol <dovalekarolina@gmail.com>
2024-12-13 01:22:55 -0300

    🌱 feat: service votos

--- src/serviceLogin.js
+++ src/serviceLogin.js
@@ -43,7 +43,7 @@ export const gerarAutenticacao = async (req, res) => {
             if (result.rows[0]) {
                 if (result.rows[0].token) {
                     const consultarVotos = await verificarVoto()
-                    return res.status(200).json({ token: result.rows[0].token, votos: consultarVotos});
+                    return res.status(200).json({ usuarioId: result.rows[0].id, token: result.rows[0].token, votos: consultarVotos});
                 }
                 const token = await gerarToken(result.rows[0].id);
 
@@ -67,44 +67,15 @@ export const gerarAutenticacao = async (req, res) => {
 };
 
 
-// export const gerarAutenticacao = async (req, res) => {
-//     const { email, senha } = req.body;
-
-//     if (email && senha) {
-//         try {
-//             // Verificar se o usuário existe
-//             const result = await bd.query('SELECT * FROM usarios WHERE email = $1 AND senha = $2', [email, senha]);
-
-//             if (result.rows[0]) {
-//                 // Verificar se o usuário já tem um token
-//                 if (result.rows[0].token) {
-//                     return res.status(200).json({ token: result.rows[0].token });
-//                 }
-
-//                 // Caso o usuário não tenha token, gerar um novo
-//                 const token = await gerarToken(result.rows[0].id);
-
-//                 return res.status(200).json({ token });
-//             } else {
-//                 res.status(401).json({ error: 'Credenciais inválidas' });
-//             }
-//         } catch (err) {
-//             console.error(err);
-//             res.status(500).json({ error: 'Erro ao processar sua solicitação' });
-//         } finally {
-//             bd.release();
-//         }
-//     } else {
-//         res.status(400).json({ error: 'Usuário não cadastrado!' });
-//     }
-// }
-
 export const criarVotacao = async (req, res) => {
-    const { diretor, filme, token } = req.body;
+    const { diretor, filme, usuarioId } = req.body;
 
-    if (diretor && filme && token) {
+    if (diretor && filme && usuarioId) {
         try {
-            await bd.query('INSERT INTO votos (diretor, filme, token) VALUES ($1, $2, $3)', [diretor, filme, token]);
+            const token = null
+            await bd.query('INSERT INTO votos (diretor, filme, usuarioId) VALUES ($1, $2, $3)', [diretor, filme, usuarioId]);
+
+            await bd.query('UPDATE usarios SET token = $1 WHERE id = $2', [token, usuarioId]);
 
             res.status(201).json({ message: 'Votação criada com sucesso!' });
         } catch (err) {
