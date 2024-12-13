2f8b2ef551cce5193323a7cfd932243010a3ffa7
Ana Karolina dos Santos do Vale <anavale@rpc.com.br>
2024-12-13 11:12:21 -0300

    🐛 fix: rota para service de votação

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
-import {gerarAutenticacao} from "./serviceLogin.js"
+import { gerarAutenticacao, criarVotacao } from "./serviceLogin.js";
 
-export const fazerLogin = (app)=>{
-    app.post('/login', async (req, res) => {await gerarAutenticacao(req, res)})
-}
+export const fazerLogin = (app) => {
+	app.post("/login", async (req, res) => {
+		await gerarAutenticacao(req, res);
+	});
+};
 
-export const votar = (app)=>{
-    app.post('/votar', async (req, res) => {await gerarAutenticacao(req, res)})
-}
+export const votar = (app) => {
+	app.post("/votar", async (req, res) => {
+		await criarVotacao(req, res);
+	});
+};
