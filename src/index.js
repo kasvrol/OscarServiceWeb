bf88c8e80d49a3b79a3e5e382189e58dc8ac7058
Ana Karolina dos Santos do Vale <anavale@rpc.com.br>
2024-12-13 14:18:45 -0300

    🐛 fix: rota de votação

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import express from "express"
-import { fazerLogin } from './routes.js'
+import express from "express";
+import { fazerLogin, votar } from "./routes.js";
 
-const app = express()
+const app = express();
 
 app.use(express.json());
 
-app.get('/', async (req, res) => {res.send('hello word')})
-fazerLogin(app)
-
+app.get("/", async (req, res) => {
+	res.send("hello word");
+});
+fazerLogin(app);
+votar(app);
 app.listen(3000, () => {
-    console.log('Server started on port 3000');
+	console.log("Server started on port 3000");
 });
--- src/serviceLogin.js
+++ src/serviceLogin.js
@@ -1,88 +1,168 @@
-import { bd } from './postgres.js'
+import { bd } from "./postgres.js";
 
-const gerarNumero = () => {
-    const numero = Math.round(Math.random() * (100 - 1)) + 1;
-    return numero;
-}
-
-const gerarToken = async (id) => {
-    let token;
-    let tokenExists = true;
-    
-    while (tokenExists) {
-        token = gerarNumero();
-        
-        const result = await bd.query('SELECT token FROM usarios WHERE token = $1', [token]);
-        
-        if (result.rows.length === 0) {
-            tokenExists = false;
-        }
-    }
-
-    await bd.query('UPDATE usarios SET token = $1 WHERE id = $2', [token, id]);
-
-    return token;
-}
-
-const verificarVoto = async (id) => {
-    const result = await bd.query('SELECT * FROM votos WHERE id = $1', [id]);
-
-    return result.rows[0];
-}
-
-
-export const gerarAutenticacao = async (req, res) => {
-
-    const { login, senha } = req.body;
-
-    if (login && senha) {
-        const client = await bd.connect();
-        try {
-            const result = await client.query('SELECT * FROM usarios WHERE login = $1 AND senha = $2', [login, senha]);
-
-            if (result.rows[0]) {
-                if (result.rows[0].token) {
-                    const consultarVotos = await verificarVoto()
-                    return res.status(200).json({ usuarioId: result.rows[0].id, token: result.rows[0].token, votos: consultarVotos});
-                }
-                const token = await gerarToken(result.rows[0].id);
-
-                return res.status(200).json({ token, votos: null });
-            }else {
-                res.status(401).json({ error: 'Credenciais inválidas' });
-            }
-        } catch (err) {
-            console.error(err);
-                    if (err instanceof Error) {
-                        res.status(500).json({ error: err.message }); 
-                    } else {
-                        res.status(500).json({ error: 'Erro desconhecido ao processar sua solicitação' });
-                   }
-        } finally {
-            client.release();
-        }
-    } else {
-        res.status(400).json({ error: 'Usuário não cadastrado!' });
-    }
+const retryConnect = async (maxAttempts = 5, delay = 2000) => {
+	let attempt = 0;
+	while (attempt < maxAttempts) {
+		try {
+			const client = await bd.connect();
+			return client;
+		} catch (err) {
+			attempt++;
+			console.log(
+				`Attempt ${attempt} failed: ${err.message}`
+			);
+			if (attempt >= maxAttempts) {
+				throw new Error(
+					"Failed to connect after multiple attempts"
+				);
+			}
+			await new Promise((resolve) =>
+				setTimeout(resolve, delay)
+			);
+		}
+	}
 };
 
+const gerarNumero = () => {
+	const numero = Math.round(Math.random() * (100 - 1)) + 1;
+	return numero;
+};
+
+const gerarToken = async (id) => {
+	let token;
+	let tokenExists = true;
+
+	while (tokenExists) {
+		token = gerarNumero();
+
+		const result = await bd.query(
+			"SELECT token FROM usarios WHERE token = $1",
+			[token]
+		);
+
+		if (result.rows.length === 0) {
+			tokenExists = false;
+		}
+	}
+
+	await bd.query("UPDATE usarios SET token = $1 WHERE id = $2", [
+		token,
+		id,
+	]);
+
+	return token;
+};
+
+const verificarVoto = async (id) => {
+	const result = await bd.query("SELECT * FROM votos WHERE id = $1", [
+		id,
+	]);
+
+	return result.rows[0];
+};
+
+export const gerarAutenticacao = async (req, res) => {
+	const { login, senha } = req.body;
+
+	if (login && senha) {
+		const client = await bd.connect();
+		try {
+			const result = await client.query(
+				"SELECT * FROM usarios WHERE login = $1 AND senha = $2",
+				[login, senha]
+			);
+
+			if (result.rows[0]) {
+				if (result.rows[0].token) {
+					const consultarVotos =
+						await verificarVoto();
+					return res.status(200).json({
+						usuarioId: result.rows[0].id,
+						token: result.rows[0].token,
+						votos: consultarVotos,
+					});
+				}
+				const token = await gerarToken(
+					result.rows[0].id
+				);
+
+				return res
+					.status(200)
+					.json({ token, votos: null });
+			} else {
+				res.status(401).json({
+					error: "Credenciais inválidas",
+				});
+			}
+		} catch (err) {
+			console.error(err);
+			if (err instanceof Error) {
+				res.status(500).json({ error: err.message });
+			} else {
+				res.status(500).json({
+					error: "Erro desconhecido ao processar sua solicitação",
+				});
+			}
+		} finally {
+			client.release();
+		}
+	} else {
+		res.status(400).json({ error: "Usuário não cadastrado!" });
+	}
+};
+
+const verificarVotacao = async (client, id) => {
+	const result = await client.query(
+		"SELECT * FROM votos WHERE usuarioId = $1",
+		[id]
+	);
+	return result.rows[0];
+};
 
 export const criarVotacao = async (req, res) => {
-    const { diretor, filme, usuarioId } = req.body;
+	const { diretor, filme, usuarioId } = req.body;
 
-    if (diretor && filme && usuarioId) {
-        try {
-            const token = null
-            await bd.query('INSERT INTO votos (diretor, filme, usuarioId) VALUES ($1, $2, $3)', [diretor, filme, usuarioId]);
+	if (diretor && filme && usuarioId) {
+		const client = await retryConnect();
+		try {
+			const jaVotou = await verificarVotacao(
+				client,
+				usuarioId
+			);
+			if (jaVotou) {
+				return res.status(409).json({
+					message: "Usuário já votou!",
+				});
+			}
 
-            await bd.query('UPDATE usarios SET token = $1 WHERE id = $2', [token, usuarioId]);
+			const token = null;
+			await client.query(
+				"INSERT INTO votos (diretor, filme, usuarioId) VALUES ($1, $2, $3)",
+				[diretor, filme, usuarioId]
+			);
 
-            res.status(201).json({ message: 'Votação criada com sucesso!' });
-        } catch (err) {
-            console.error(err);
-            res.status(500).json({ error: 'Erro ao criar votação' });
-        }
-    } else {
-        res.status(400).json({ error: 'Dados incompletos para criar votação' });
-    }
-}
+			await client.query(
+				"UPDATE usarios SET token = $1 WHERE id = $2",
+				[token, usuarioId]
+			);
+
+			res.status(201).json({
+				message: "Votação criada com sucesso!",
+			});
+		} catch (err) {
+			if (err instanceof Error) {
+				res.status(404).json({
+					message: "Usuário não cadastrado!",
+				});
+			} else {
+				res.status(500).json({
+					error: "Erro desconhecido ao processar sua solicitação",
+				});
+			}
+		}
+	} else {
+		res.status(400).json({
+			error: "Dados incompletos para criar votação",
+		});
+	}
+};
