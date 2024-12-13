b6aa7eeaa3c21cd3703430896c3768485358b6de
kasvrol <dovalekarolina@gmail.com>
2024-12-12 23:40:35 -0300

    🚧 feat: configuração inicial do servidor

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from "express"
+import { fazerLogin } from './src/routes.js'
 
 const app = express()
 
+app.use(express.json());
+
+fazerLogin(app)
+
 app.listen(3000, () => {
     console.log('Server started on port 3000');
-  });
+});
