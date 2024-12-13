288651521d538e5df828265ec110742de55bdf90
kasvrol <dovalekarolina@gmail.com>
2024-12-12 23:42:00 -0300

    🏦 feat: config postgres

---
+++ src/postgres.js
@@ -0,0 +1,18 @@
+import pkg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const { Pool } = pkg;
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+export const bd = new Pool({
+  host: PGHOST,
+  database: PGDATABASE,
+  user: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: {
+    require: true,
+  },
+});
+
